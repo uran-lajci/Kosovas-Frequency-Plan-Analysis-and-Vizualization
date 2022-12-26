51c6c28d037662aca55b44887242544605d0ddbe
uran-lajci <117693854+uran-lajci@users.noreply.github.com>
2022-12-26 20:04:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Titulli - Frequency")
-df = pd.read_csv('C:\\Users\\uran_\\frequenct_data.csv')
+df = pd.read_csv('C:\\Users\\uran_\\frequency.csv')
 
+location = df['Location'].unique()
+language = df['Language'].unique()
 
 with st.form(key='salaryform'):
     col1,col2,col3, col4 = st.columns(4)
     
     with col1:
-        option1 = st.selectbox('Frequency bands',(
+        frequencyBands = st.selectbox('Frequency bands',(
             '3 - 30 kHz',
             '30 - 300 kHz',
             '300 kHz - 3 MHz',
@@ -24,24 +26,9 @@ with st.form(key='salaryform'):
             '3 - 30 GHz',
             '30 - 300 GHz'))
     with col2:
-        option2 = st.selectbox('Frequency table',(
-            'Albania',
-            'Austria',
-            'Belgium',
-            'Croatia',
-            'France',
-            'Germany',
-            'Greece',
-            'Portugal'))
+        state = st.selectbox('Frequency table', options=list(location))
     with col3:
-        option3 = st.selectbox('Language',(
-            'Albanian',
-            'Bosnian',
-            'Croation',
-            'English',
-            'German',
-            'Greek',
-            'Portugal'))
+        languageFromDataset = st.selectbox('Language', options=list(language))
     with col4:
         searchType = st.radio("Search Type", ("Application", "Alocation"))
         check = st.checkbox("Compare (Select multiple frequency tables)")
@@ -50,12 +37,35 @@ with st.form(key='salaryform'):
     submit_salary = st.form_submit_button(label='Search')
 
 if submit_salary:
-    st.write(option1)
-    st.write(option2)
-    st.write(option3)
-    st.write(searchType)
+    if frequencyBands == "3 - 30 kHz":
+        lowerBound = 3
+        upperBound = 30
+    elif frequencyBands == "30 - 300 kHz":
+        lowerBound = 30
+        upperBound = 300
+    elif frequencyBands == "300 kHz - 3 MHz":
+       lowerBound = 300
+       upperBound = 30000
+    elif frequencyBands == "3 - 30 MHz":
+       lowerBound = 30000 
+       upperBound = 300000 
+    elif frequencyBands == "30 MHz - 300 MHz":
+       lowerBound = 300000 
+       upperBound = 3000000 
+    elif frequencyBands == "300 MHz - 3 GHz":
+       lowerBound = 300000
+       upperBound = 3000000
+    elif frequencyBands == "3 - 30 GHz":
+       lowerBound = 3000000
+       upperBound = 30000000
+    elif frequencyBands == "'30 - 300 GHz'":
+       lowerBound = 30000000
+       upperBound = 300000000
+    
+    st.write(df[(df['Lower Frequency'] >= lowerBound) & (df['Lower Frequency'] <= upperBound) 
+        & (df['Location'] == state)
+        & (df['Language'] == languageFromDataset)
+        & (df['Type'] == searchType)    
+    ])
     st.write(check)
     st.write(allocationOrientation)
-
-    
-
