0bc2be691455409e62dbcd237d0859da63b5d86e
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-01-09 13:01:01 +0100

    changed format of file

--- frequency_bounds_graph.py
+++ frequency_bounds_graph.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import squarify
 import pandas as pd
 import random
-import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
@@ -15,136 +13,161 @@ volume = df["_lowerFrequency"].loc[0:10]
 labels = len(df["_term"].unique())
 l = df["_term"]
 
-colors = ["aqua","aquamarine","azure","beige","bisque","blanchedalmond","blue","blueviolet","brown",
-"burlywood","cadetblue","chartreuse","chocolate","coral"
-,"cornflowerblue","cornsilk","crimson","cyan","darkblue"
-,"darkcyan"
-,"darkgoldenrod"
-,"darkgray"
-,"darkgreen"
-,"darkkhaki"
-,"darkmagenta"
-,"darkolivegreen"
-,"darkorange"
-,"darkorchid"
-,"darkred"
-,"darksalmon"
-,"darkseagreen"
-,"darkslateblue"
-,"darkslategray"
-,"darkturquoise"
-,"darkviolet"
-,"deeppink"
-,"deepskyblue"
-,"dimgray"
-,"dodgerblue"
-,"firebrick"
-,"floralwhite"
-,"forestgreen"
-,"fuchsia"
-,"gainsboro"
-,"ghostwhite"
-,"gold"
-,"goldenrod"
-,"gray"
-,"green"
-,"greenyellow"
-,"honeydew"
-,"hotpink"
-,"indianred"
-,"indigo"
-,"ivory"
-,"khaki"
-,"lavender"
-,"lavenderblush"
-,"lawngreen"
-,"lemonchiffon"
-,"lightblue"
-,"lightcoral"
-,"lightcyan"
-,"lightgoldenrodyellow"
-,"lightgreen"
-,"lightgrey"
-,"lightpink"
-,"lightsalmon"
-,"lightseagreen"
-,"lightskyblue"
-,"lightslategray"
-,"lightsteelblue"
-,"lightyellow"
-,"lime"
-,"limegreen"
-,"linen"
-,"magenta"
-,"maroon"
-,"mediumaquamarine"
-,"mediumblue"
-,"mediumorchid"
-,"mediumpurple"
-,"mediumseagreen"
-,"mediumslateblue"
-,"mediumspringgreen"
-,"mediumturquoise"
-,"mediumvioletred"
-,"midnightblue"
-,"mintcream"
-,"mistyrose"
-,"moccasin"
-,"navajowhite"
-,"navy"
-,"navyblue"
-,"oldlace"
-,"olive"
-,"olivedrab"
-,"orange"
-,"orangered"
-,"orchid"
-,"palegoldenrod"
-,"palegreen"
-,"paleturquoise"
-,"palevioletred"
-,"papayawhip"
-,"peachpuff"
-,"peru"
-,"pink"
-,"plum"
-,"powderblue"
-,"purple"
-,"red"
-,"rosybrown"
-,"royalblue"
-,"saddlebrown"
-,"salmon"
-,"sandybrown"
-,"seagreen"
-,"seashell"
-,"sienna"
-,"silver"
-,"skyblue"
-,"slateblue"
-,"slategray"
-,"snow"
-,"springgreen"
-,"steelblue"
-,"tan"
-,"teal"
-,"thistle"
-,"tomato"
-,"turquoise","violet","wheat"
-,"white","whitesmoke"
-,"yellow","yellowgreen"]
+colors = [
+    "aqua",
+    "aquamarine",
+    "azure",
+    "beige",
+    "bisque",
+    "blanchedalmond",
+    "blue",
+    "blueviolet",
+    "brown",
+    "burlywood",
+    "cadetblue",
+    "chartreuse",
+    "chocolate",
+    "coral",
+    "cornflowerblue",
+    "cornsilk",
+    "crimson",
+    "cyan",
+    "darkblue",
+    "darkcyan",
+    "darkgoldenrod",
+    "darkgray",
+    "darkgreen",
+    "darkkhaki",
+    "darkmagenta",
+    "darkolivegreen",
+    "darkorange",
+    "darkorchid",
+    "darkred",
+    "darksalmon",
+    "darkseagreen",
+    "darkslateblue",
+    "darkslategray",
+    "darkturquoise",
+    "darkviolet",
+    "deeppink",
+    "deepskyblue",
+    "dimgray",
+    "dodgerblue",
+    "firebrick",
+    "floralwhite",
+    "forestgreen",
+    "fuchsia",
+    "gainsboro",
+    "ghostwhite",
+    "gold",
+    "goldenrod",
+    "gray",
+    "green",
+    "greenyellow",
+    "honeydew",
+    "hotpink",
+    "indianred",
+    "indigo",
+    "ivory",
+    "khaki",
+    "lavender",
+    "lavenderblush",
+    "lawngreen",
+    "lemonchiffon",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgreen",
+    "lightgrey",
+    "lightpink",
+    "lightsalmon",
+    "lightseagreen",
+    "lightskyblue",
+    "lightslategray",
+    "lightsteelblue",
+    "lightyellow",
+    "lime",
+    "limegreen",
+    "linen",
+    "magenta",
+    "maroon",
+    "mediumaquamarine",
+    "mediumblue",
+    "mediumorchid",
+    "mediumpurple",
+    "mediumseagreen",
+    "mediumslateblue",
+    "mediumspringgreen",
+    "mediumturquoise",
+    "mediumvioletred",
+    "midnightblue",
+    "mintcream",
+    "mistyrose",
+    "moccasin",
+    "navajowhite",
+    "navy",
+    "navyblue",
+    "oldlace",
+    "olive",
+    "olivedrab",
+    "orange",
+    "orangered",
+    "orchid",
+    "palegoldenrod",
+    "palegreen",
+    "paleturquoise",
+    "palevioletred",
+    "papayawhip",
+    "peachpuff",
+    "peru",
+    "pink",
+    "plum",
+    "powderblue",
+    "purple",
+    "red",
+    "rosybrown",
+    "royalblue",
+    "saddlebrown",
+    "salmon",
+    "sandybrown",
+    "seagreen",
+    "seashell",
+    "sienna",
+    "silver",
+    "skyblue",
+    "slateblue",
+    "slategray",
+    "snow",
+    "springgreen",
+    "steelblue",
+    "tan",
+    "teal",
+    "thistle",
+    "tomato",
+    "turquoise",
+    "violet",
+    "wheat",
+    "white",
+    "whitesmoke",
+    "yellow",
+    "yellowgreen",
+]
 
 html_string = ["<div class='container'>"]
 
 for i in range(labels):
-    html_string.append(f'''
+    html_string.append(
+        f"""
     <div style="width:200px;height:150px;background-color:{colors[i]};display:inline-block;line-height:2.5;padding:20px">
         <h5>{l.loc[i]}</h5>
         <p>{df["_lowerFrequency"].loc[i]} - {df["_higherFrequency"].loc[i]}</p>
     </div>
-    ''')
+    """
+    )
 
-html_string.append(''' 
+html_string.append(
+    """ 
 
 </div>
 <style>
@@ -154,5 +177,6 @@ html_string.append('''
     white-space: nowrap;
 }
 </style>
-''')
+"""
+)
 components.html("".join(html_string))  # JavaScript works
