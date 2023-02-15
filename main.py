ede36db4363201b79dcd7b81c6dfef51162cb514
uranlajci <117693854+uran-lajci@users.noreply.github.com>
2023-02-15 03:17:35 +0100

    removed unused imports from main

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from utils.slider_utils import *
-from utils.general_utils import *
 from modules.frequency_slider import *
 from modules.frequency_filter import *
 from modules.statistics import *
@@ -15,6 +13,7 @@ dataset = pd.read_csv('FP_KOS_2022.csv')
 dataset = dataset.rename(columns={'_term': 'service'})
 
 tabForFrequencySlider, tabForFrequencyFilter, tabForStatistics, tabForAbout = st.tabs(["Frequency Slider", "Frequency Filter", "Statistics", "About"])
+
 with tabForFrequencySlider:
     getFrequencySlider(dataset)
 
